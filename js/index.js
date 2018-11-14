b4379071a586c79e820a8a8c5a0dd35b3e581f66
譚詩朗 <sonia1997nt@gmail.com>
2018-11-14 23:55:57 +0800

    Update index page

--- js/index.js
+++ js/index.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
         else
             $(this).text('More Options');
     });
+
+    $(".index_amount_item").click(function(){
+        var num = $(this).text();
+        if(num != "del"){
+            var inputAmount = $(".amount_input").val();
+            $(".amount_input").val(inputAmount + num);
+        }
+        else if(num == "del"){
+            var amount = $(".amount_input").val();
+            var deleted = amount.substring(0, amount.length - 1);
+            $(".amount_input").val(deleted);
+        }
+
+    });
+
+
 });

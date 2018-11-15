98670e418c81e2ed58b29b5c910dd742516341e0
ACTony <actony@outlook.com>
2018-11-15 17:13:44 +0800

    update api.js class

--- js/api.js
+++ js/api.js
@@ -1,5 +1,29 @@
 class CurrencyAPI {
+    constructor(targetCurrency = "USD") {
+        this.targetCurrency = targetCurrency;
+    }
 
+    async fetchAll() {
+        try {
+            var endpoint = `http://35.241.126.74/api/get-rate?cur_code=${this.targetCurrency}`;
+            var result = await fetch(endpoint, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json"
+                },
+            }).then(res => res.json());
+            var bankCurrencyDictionary = {};
+            result.forEach((rec) => {
+                if (bankCurrencyDictionary[rec.bank]) return;
+                bankCurrencyDictionary[rec.bank] = rec.rate;
+            });
+            return bankCurrencyDictionary;
+        } catch (exception) {
+            console.error(`[ERROR] Exception catched in CurrencyAPI::fetchAll.`);
+            console.error(exception);
+            return {};
+        }
+    }
 }
 
 window.CurrencyAPI = CurrencyAPI;
